65a0f9401d47fc69066647c34966fc6c9a759d99
WintermeW <clement@digi-nation.com>
2011-01-05 01:01:22 +0100

    - Changed web engine from gtkmozembed to webkit (much more stable) - Finished implementation of contextual Help ( need to finish the doc pages though ) - Started iplementation of synerpreter, good progresses on that (synapse workflow schemes interpreter CLI mode)

--- src/lib/synapseIMVEC.py
+++ src/lib/synapseIMVEC.py
@@ -1,6 +1,12 @@
-import pygtk
-pygtk.require("2.0")
-import gtk
+try:
+   import pygtk
+   pygtk.require("2.0")
+   import gtk
+   HAS_GTK = 1
+except:
+   HAS_GTK = 0
+   pass
+
 import os
 from synapseDebug import dbg
 from synapseEngine import synapseEngine
@@ -12,12 +18,14 @@ class IMVEC:
 
    dbg = None;
    cfg = None;
+
+   has_webkit = 0
    
-   #application clipboard
-   cb =  gtk.Clipboard(gtk.gdk.display_get_default(),"CLIPBOARD")
+   #application clipboard. loads only if gtk available
+   if HAS_GTK:
+      cb =  gtk.Clipboard(gtk.gdk.display_get_default(),"CLIPBOARD")
 
    engine = None
-
 
    #gtk widget references shared by both main and canvas modules
    obrowser = None
@@ -45,84 +53,84 @@ class IMVEC:
    pkgMgr = None
    libtree = None
 
+   if HAS_GTK:
+      folderPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/folder.png" % (RES_PATH),16,16)
+      wfPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/synwf.png" % (RES_PATH),16,16)
 
-   folderPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/folder.png" % (RES_PATH),16,16)
-   wfPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/synwf.png" % (RES_PATH),16,16)
+      appPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/synappItem.png" % (RES_PATH),32,32)
+      servPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/servItem.png" % (RES_PATH),32,32)
+      timerPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/timerItem.png" % (RES_PATH),32,32)
+      injectorPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/injectItem.png" % (RES_PATH),32,32)
+      testPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/testItem.png" % (RES_PATH),32,32)
+      demuxPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/demuxItem.png" % (RES_PATH),32,32)
+      muxPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/muxItem.png" % (RES_PATH),32,32)
+      commentPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/commentItem.png" % (RES_PATH),32,32)
+      filterPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/filterItem.png" % (RES_PATH),32,32)
+      monitorPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/monitorItem.png" % (RES_PATH),32,32)
+      containerPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/containerItem.png" % (RES_PATH),32,32)
+      reportPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/reportItem.png" % (RES_PATH),32,32)
+      dbPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/dbItem.png" % (RES_PATH),32,32)
 
-   appPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/synappItem.png" % (RES_PATH),32,32)
-   servPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/servItem.png" % (RES_PATH),32,32)
-   timerPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/timerItem.png" % (RES_PATH),32,32)
-   injectorPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/injectItem.png" % (RES_PATH),32,32)
-   testPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/testItem.png" % (RES_PATH),32,32)
-   demuxPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/demuxItem.png" % (RES_PATH),32,32)
-   muxPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/muxItem.png" % (RES_PATH),32,32)
-   commentPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/commentItem.png" % (RES_PATH),32,32)
-   filterPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/filterItem.png" % (RES_PATH),32,32)
-   monitorPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/monitorItem.png" % (RES_PATH),32,32)
-   containerPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/containerItem.png" % (RES_PATH),32,32)
-   reportPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/reportItem.png" % (RES_PATH),32,32)
-   dbPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/dbItem.png" % (RES_PATH),32,32)
-
-   kbdPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/keyboard.png" % (RES_PATH),32,32)
-   kbdPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/keyboard.png" % (RES_PATH),16,16)
+      kbdPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/keyboard.png" % (RES_PATH),32,32)
+      kbdPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/keyboard.png" % (RES_PATH),16,16)
    
 
 
-   labelPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/labelItem.png" % (RES_PATH),32,32)
-   exportPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/32/export.png" % (RES_PATH),32,32)
-   exportOverPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/32/export_over.png" % (RES_PATH),32,32)
-   cplayPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cplay.png" % (RES_PATH),32,32)
-   cplayOverPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cplay_over.png" % (RES_PATH),32,32)
-   cstopPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cstop.png" % (RES_PATH),32,32)
-   cstopOverPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cstop_over.png" % (RES_PATH),32,32)
+      labelPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/labelItem.png" % (RES_PATH),32,32)
+      exportPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/32/export.png" % (RES_PATH),32,32)
+      exportOverPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/32/export_over.png" % (RES_PATH),32,32)
+      cplayPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cplay.png" % (RES_PATH),32,32)
+      cplayOverPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cplay_over.png" % (RES_PATH),32,32)
+      cstopPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cstop.png" % (RES_PATH),32,32)
+      cstopOverPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/cstop_over.png" % (RES_PATH),32,32)
 
    
-   appPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/synappItem.png" % (RES_PATH),16,16)
-   servPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/servItem.png" % (RES_PATH),16,16)
-   timerPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/timerItem.png" % (RES_PATH),16,16)
-   injectorPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/injectItem.png" % (RES_PATH),16,16)
-   testPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/testItem.png" % (RES_PATH),16,16)
-   demuxPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/demuxItem.png" % (RES_PATH),16,16)
-   muxPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/muxItem.png" % (RES_PATH),16,16)
-   commentPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/commentItem.png" % (RES_PATH),16,16)
-   filterPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/filterItem.png" % (RES_PATH),16,16)
-   monitorPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/monitorItem.png" % (RES_PATH),16,16)
-   containerPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/containerItem.png" % (RES_PATH),16,16)
-   reportPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/reportItem.png" % (RES_PATH),16,16)
-   dbPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/dbItem.png" % (RES_PATH),16,16)  
+      appPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/synappItem.png" % (RES_PATH),16,16)
+      servPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/servItem.png" % (RES_PATH),16,16)
+      timerPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/timerItem.png" % (RES_PATH),16,16)
+      injectorPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/injectItem.png" % (RES_PATH),16,16)
+      testPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/testItem.png" % (RES_PATH),16,16)
+      demuxPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/demuxItem.png" % (RES_PATH),16,16)
+      muxPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/muxItem.png" % (RES_PATH),16,16)
+      commentPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/commentItem.png" % (RES_PATH),16,16)
+      filterPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/filterItem.png" % (RES_PATH),16,16)
+      monitorPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/monitorItem.png" % (RES_PATH),16,16)
+      containerPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/containerItem.png" % (RES_PATH),16,16)
+      reportPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/reportItem.png" % (RES_PATH),16,16)
+      dbPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/dbItem.png" % (RES_PATH),16,16)  
  
    
 
-   labelPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/labelItem.png" % (RES_PATH),16,16)   
+      labelPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/labelItem.png" % (RES_PATH),16,16)   
 
 
-   resizePixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/32/resize.png" % (RES_PATH),16,16)
+      resizePixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/32/resize.png" % (RES_PATH),16,16)
 
-   monitorMaximizePixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/16/monMax.xpm" % (RES_PATH),16,16)
-   monitorHidePixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/16/monHide.xpm" % (RES_PATH),16,16)
+      monitorMaximizePixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/16/monMax.xpm" % (RES_PATH),16,16)
+      monitorHidePixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/16/monHide.xpm" % (RES_PATH),16,16)
 
  
-   linkPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/256/linkItem.png" % (RES_PATH),16,16)
-   synapse_logo = gtk.gdk.pixbuf_new_from_file("%s/images/synapse.png" % (RES_PATH))
-   loopPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/loop.png" % (RES_PATH),16,16)  
-   cpuPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/48/cpu.png" % (RES_PATH),16,16) 
+      linkPixbuf_s = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/256/linkItem.png" % (RES_PATH),16,16)
+      synapse_logo = gtk.gdk.pixbuf_new_from_file("%s/images/synapse.png" % (RES_PATH))
+      loopPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/128/loop.png" % (RES_PATH),16,16)  
+      cpuPixbuf = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/48/cpu.png" % (RES_PATH),16,16) 
 
 
-   ioleft = gtk.gdk.pixbuf_new_from_file("%s/images/io_left.png" % (RES_PATH));
-   ioright = gtk.gdk.pixbuf_new_from_file("%s/images/io_right.png" % (RES_PATH));
-   iotop = gtk.gdk.pixbuf_new_from_file("%s/images/io_top.png" % (RES_PATH));
-   iobottom = gtk.gdk.pixbuf_new_from_file("%s/images/io_bottom.png" % (RES_PATH));
+      ioleft = gtk.gdk.pixbuf_new_from_file("%s/images/io_left.png" % (RES_PATH));
+      ioright = gtk.gdk.pixbuf_new_from_file("%s/images/io_right.png" % (RES_PATH));
+      iotop = gtk.gdk.pixbuf_new_from_file("%s/images/io_top.png" % (RES_PATH));
+      iobottom = gtk.gdk.pixbuf_new_from_file("%s/images/io_bottom.png" % (RES_PATH));
 
-   bbhoriz = gtk.gdk.pixbuf_new_from_file("%s/images/bb_horiz.png" % (RES_PATH));
+      bbhoriz = gtk.gdk.pixbuf_new_from_file("%s/images/bb_horiz.png" % (RES_PATH));
 
 
-   #Help Browser Icons
-   arrow_l = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/arrow_l.png" % (RES_PATH),20,20)
-   arrow_r = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/arrow_r.png" % (RES_PATH),32,20)
-   doc_search = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/search.png" % (RES_PATH),20,20)   
+      #Help Browser Icons
+      arrow_l = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/arrow_l.png" % (RES_PATH),20,20)
+      arrow_r = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/arrow_r.png" % (RES_PATH),32,20)
+      doc_search = gtk.gdk.pixbuf_new_from_file_at_size("%s/icons/64/search.png" % (RES_PATH),20,20)   
    
 
 
-   extendCursor = gtk.gdk.Cursor(gtk.gdk.BOTTOM_RIGHT_CORNER)
-   plusCursor = gtk.gdk.Cursor(gtk.gdk.PLUS)
-   crossCursor = gtk.gdk.Cursor(gtk.gdk.TCROSS)
+      extendCursor = gtk.gdk.Cursor(gtk.gdk.BOTTOM_RIGHT_CORNER)
+      plusCursor = gtk.gdk.Cursor(gtk.gdk.PLUS)
+      crossCursor = gtk.gdk.Cursor(gtk.gdk.TCROSS)
